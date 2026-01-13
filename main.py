40dd8d80acf0630365ebc4ad16b8adc8f1e39663
Luca Ognibene <luca.ognibene@gmail.com>
2026-01-13 19:18:52 +0100

    clear a bit

--- main.py
+++ main.py
@@ -4,66 +4,81 @@ from kivy.clock import Clock
 from kivy.properties import NumericProperty, StringProperty
 from kivy.utils import platform
 import math
+from typing import override
 
 __version__ = "0.1"
 
-# https://stackoverflow.com/questions/51551081/how-to-find-usb-devices-and-communicate-with-them-using-pyserial-in-android
-# https://groups.google.com/g/kivy-users/c/GcN15VS3NA0
-# https://github.com/jacklinquan/usbserial4a
-# https://github.com/markqvist/Sideband/blob/main/sbapp/Makefile
-# https://github.com/frmdstryr/kivy-android-serial
-
 
 class HW:
-    def __init__(self):
+    def __init__(self) -> None:
         self.load()
 
-    def check(self):
+    def check(self) -> None:
         if self.device is None or self.hardware is None:
             self.load()
 
     def load(self):
         self.device = None
         self.hardware = None
+        self.hw = None
 
-        if platform == "linux":
-            import serial
+    def get_compass(self) -> tuple[int, int]:
+        o_turn, o_power = 0, 0
+        return o_turn, o_power
 
-            try:
-                self.device = serial.Serial("/dev/ttyUSB0", 57600, timeout=1)
-                self.hardware = True
-            except Exception as e:
-                print("Disable hardware support")
-                print(e)
+    def send_data(self, data, timeout):
+        if self.device:
+            self.device.write(data.encode())
 
-        elif platform == "android":
-            try:
-                from usb4a import usb
-                from usbserial4a import serial4a
-                from plyer import spatialorientation
+    def stop(self):
+        pass
 
-                spatialorientation.enable_listener()
-                self.hardware = spatialorientation
+    def start(self):
+        pass
 
-                usb_device_list = usb.get_usb_device_list()
-                device_name_list = [
-                    device.getDeviceName() for device in usb_device_list
-                ]
-                print("DEVICS", device_name_list)
-                device_name = device_name_list[0]
-                device = usb.get_usb_device(device_name)
-                if not usb.has_usb_permission(device):
-                    print("ASKING FOR USB PERMISSION")
-                    usb.request_usb_permission(device)
-                    return
-                self.device = serial4a.get_serial_port(
-                    device_name, 57600, 8, "N", 1, timeout=1
-                )
-            except Exception as e:
-                print("Disable hardware support")
-                print(e)
 
-    def get_compass(self):
+class HWAndroid(HW):
+
+    @override
+    def load(self) -> None:
+        HW.load(self)
+
+        try:
+            from usb4a import usb
+            from usbserial4a import serial4a
+            from plyer import spatialorientation
+
+            spatialorientation.enable_listener()
+            self.hardware = spatialorientation
+
+            usb_device_list = usb.get_usb_device_list()
+            device_name_list = [device.getDeviceName() for device in usb_device_list]
+            print("DEVICS", device_name_list)
+            device_name = device_name_list[0]
+            device = usb.get_usb_device(device_name)
+            if not usb.has_usb_permission(device):
+                print("ASKING FOR USB PERMISSION")
+                usb.request_usb_permission(device)
+                return
+            self.device = serial4a.get_serial_port(
+                device_name, 57600, 8, "N", 1, timeout=1
+            )
+        except Exception as e:
+            print("Disable hardware support")
+            print(e)
+
+    @override
+    def start(self):
+        if self.hardware:
+            self.hardware.enable_listener()
+
+    @override
+    def stop(self):
+        if platform == "android" and self.hardware:
+            self.hardware.disable_listener()
+
+    @override
+    def get_compass(self) -> tuple[int, int]:
         """
         On Android, read phone orientation and scale values for control:
         - pitch (values[2]) in [-pi/2, 0] -> power 0..100
@@ -71,7 +86,7 @@ class HW:
         Returns (turn, power) as integers.
         """
         o_turn, o_power = 0, 0
-        if platform == "android" and self.hardware:
+        if self.hardware:
             values = self.hardware.orientation
             if values[1] is not None and values[2] is not None:
                 v_pitch = float(values[2])
@@ -82,21 +97,20 @@ class HW:
                 o_turn = int((v_roll + math.pi / 2) * (100.0 / math.pi)) - 50
         return o_turn, o_power
 
-    def send_data(self, data, timeout):
-        if self.device:
-            if platform == "android":
-                # self.device.write(data, timeout)
-                self.device.write(data.encode())
-            elif platform == "linux":
-                self.device.write(data.encode())
 
-    def stop(self):
-        if platform == "android" and self.hardware:
-            self.hardware.disable_listener()
+class HWLinux(HW):
+    @override
+    def load(self) -> None:
+        import serial
 
-    def start(self):
-        if platform == "android" and self.hardware:
-            self.hardware.enable_listener()
+        HW.load(self)
+
+        try:
+            self.device = serial.Serial("/dev/ttyUSB0", 57600, timeout=1)
+            self.hardware = True
+        except serial.SerialException as e:
+            print("Disable hardware support")
+            print(e)
 
 
 class CrazyBotGame(Widget):
@@ -107,6 +121,10 @@ class CrazyBotGame(Widget):
     m2 = StringProperty("")
     reverse = NumericProperty(0)
     device = StringProperty("nothing")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.hw: HW | None = None
 
     def get_motor_data(self, z: int, y: int, r: int):
         """
@@ -149,32 +167,41 @@ class CrazyBotGame(Widget):
         return int(left), int(right), r
 
     def send_data(self, data):
-        payload = "42,%d,119,%s," % (len(data) + 1, ",".join([str(a) for a in data]))
-        self.hw.send_data(payload, 200)
+        if self.hw:
+            payload = "42,%d,119,%s," % (
+                len(data) + 1,
+                ",".join([str(a) for a in data]),
+            )
+            self.hw.send_data(payload, 200)
 
     def update(self, dt):
-        (self.orient1, self.orient2) = self.hw.get_compass()
-        v1, v2, r = self.get_motor_data(self.orient2, self.orient1, self.reverse)
-        self.send_data([v1, v2, r])
+        if self.hw:
+            (self.orient1, self.orient2) = self.hw.get_compass()
+            v1, v2, r = self.get_motor_data(self.orient2, self.orient1, self.reverse)
+            self.send_data([v1, v2, r])
 
     def init_hw(self):
-        self.hw = HW()
+        if platform == "android":
+            self.hw = HWAndroid()
+        elif platform == "linux":
+            self.hw = HWLinux()
 
     def check_hw(self, dt):
         if self.hw:
             self.hw.check()
 
     def _stop(self):
-        self.hw.stop()
+        if self.hw:
+            self.hw.stop()
 
     def _start(self):
-        self.hw.start()
+        if self.hw:
+            self.hw.start()
 
 
 class CrazyBotApp(App):
     def build(self):
-        global SPORT
-        self._game = CrazyBotGame()
+        self._game: CrazyBotGame = CrazyBotGame()
         self._game.init_hw()
         Clock.schedule_interval(self._game.update, 1.0 / 60.0)
         Clock.schedule_interval(self._game.check_hw, 5.0)
